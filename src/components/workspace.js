1aadf70a04476809cf279c9360846a674568d2bb
Dillon Smith <dillon.th.smith@gmail.com>
2019-09-30 17:41:03 -0400

    refactored workspace so that population of layout components no longer happens in ComponentWillMount

--- src/components/workspace.js
+++ src/components/workspace.js
@@ -9,17 +9,132 @@ import ParameterControlBox from './parametercontrolbox'
 export default class Workspace extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      active_tooltip:'tea',
+        outerGrid: {
+        className: 'outergrid',
+          margin: [0, 10],
+          layout: [
+          {
+            i: 'outer_a',
+            x: 0,
+            y: 0,
+            w: 12,
+            h: 24
+          },
+          {
+            i: 'outer_b',
+            x: 0,
+            y: 25,
+            w: 12,
+            h: 8
+          },
+        ],
+          cols: 12,
+          rowHeight: 30,
+          width: 2560
+      },
+      rowOne: {
+        className: 'row_one',
+          layout: [
+          {
+            i: 'a',
+            x: 0,
+            y: 0,
+            w: 430,
+            h: 24
+          },
+          {
+            i: 'b',
+            x: 430,
+            y: 0,
+            w: 2130,
+            h: 24
+          }
+        ],
+          components: [],
+          cols: 2560,
+          rowHeight: 30,
+          width: 2560
+      },
+      rowTwo: {
+        className: 'row_two',
+          layout: [
+          {
+            i: 'c',
+            x: 0,
+            y: 25,
+            w: 430,
+            h: 8
+          },
+          {
+            i: 'd',
+            x: 430,
+            y: 25,
+            w: 2130,
+            h: 8
+          },
+        ],
+          components: [],
+          cols: 2560,
+          rowHeight: 30,
+          width: 2560
+      }
+    }
     this.componentWillMount = this.componentWillMount.bind(this)
     this.get_mouse_on_drag = this.get_mouse_on_drag.bind(this)
     this.get_mouse_initial = this.get_mouse_initial.bind(this)
     this.set_tool_tip = this.set_tool_tip.bind(this)
-    var mouse
+    this.set_components = this.set_components.bind(this)
+    this.set_components()
+  }
+
+  set_components() {
+    var self = this
+    var row_one_components = [
+      <div key="a">
+        <SideBar hover={() => this.set_tool_tip('sidebar')}
+                 className='pnl-panel'
+                 onResizeStart={this.get_mouse_initial}
+                 onResize={function () {
+                   self.get_mouse_on_drag(
+                     {
+                       'horizontal': [self.state.rowOne.components, 0],
+                       'vertical': [self.state.outerGrid.components, 0]
+                     },
+                     {
+                       'horizontal': [self.state.rowOne.layout,'rowOne'],
+                       'vertical':  [self.state.outerGrid.layout,'outerGrid']
+                     }
+                   )
+                 }}
+        />
+      </div>,
+      <div key="b">
+        <GraphView className='pnl-panel'/>
+      </div>
+    ]
+    var row_two_components = [
+      <div key="c">
+        <ToolTipBox text={this.state.active_tooltip}
+                    className='pnl-panel'/>
+      </div>,
+      <div key="d">
+        <ParameterControlBox text={this.state.active_tooltip}
+                             className='pnl-panel'/>
+      </div>
+    ]
+    var stateWithComponents = this.state
+    stateWithComponents.rowOne.components = row_one_components
+    stateWithComponents.rowTwo.components = row_two_components
+    this.setState(stateWithComponents)
   }
 
   set_tool_tip(text) {
+    var state = this.state
+    state.active_tooltip = 'fdsjafkdjafls'
     this.setState(
-      { 'active_tooltip': text }
+      state
     )
   }
 
@@ -47,7 +162,7 @@ export default class Workspace extends React.Component {
         new_layout[component_index+1].x += offset
         var state = this.state
         state[layout_key].layout = new_layout
-        self.setState({...state} )
+        self.setState(state)
         this.mouse_initial = mouse_current
         console.log(self.state)
       }
@@ -59,115 +174,11 @@ export default class Workspace extends React.Component {
   }
 
   componentWillMount() {
-    var self = this
     window.addEventListener('mousemove', (e) => {
         this.setState({mouse:e})
       }
     )
-    this.setState({
-        active_tooltip:'tea',
-        outerGrid: {
-          className: 'outergrid',
-          margin: [0, 10],
-          layout: [
-            {
-              i: 'outer_a',
-              x: 0,
-              y: 0,
-              w: 12,
-              h: 24
-            },
-            {
-              i: 'outer_b',
-              x: 0,
-              y: 25,
-              w: 12,
-              h: 8
-            },
-          ],
-          cols: 12,
-          rowHeight: 30,
-          width: 2560
-        },
-        rowOne: {
-          className: 'row_one',
-          layout: [
-            {
-              i: 'a',
-              x: 0,
-              y: 0,
-              w: 430,
-              h: 24
-            },
-            {
-              i: 'b',
-              x: 430,
-              y: 0,
-              w: 2130,
-              h: 24
-            }
-          ],
-          components: [
-            <div key="a">
-              <SideBar hover={() => this.set_tool_tip('sidebar')}
-                       className='pnl-panel'
-                       onResizeStart={this.get_mouse_initial}
-                       onResize={function () {
-                         self.get_mouse_on_drag(
-                           {
-                             'horizontal': [self.state.rowOne.components, 0],
-                             'vertical': [self.state.outerGrid.components, 0]
-                           },
-                           {
-                             'horizontal': [self.state.rowOne.layout,'rowOne'],
-                             'vertical':  [self.state.outerGrid.layout,'outerGrid']
-                           }
-                         )
-                       }}
-              />
-            </div>,
-            <div key="b">
-              <GraphView className='pnl-panel'/>
-            </div>
-          ],
-          cols: 2560,
-          rowHeight: 30,
-          width: 2560
-        },
-        rowTwo: {
-          className: 'row_two',
-          layout: [
-            {
-              i: 'c',
-              x: 0,
-              y: 25,
-              w: 430,
-              h: 8
-            },
-            {
-              i: 'd',
-              x: 430,
-              y: 25,
-              w: 2130,
-              h: 8
-            },
-          ],
-          components: [
-            <div key="c">
-              <ToolTipBox text={this.state.active_tooltip}
-                          className='pnl-panel'/>
-            </div>,
-            <div key="d">
-              <ParameterControlBox text={this.state.active_tooltip}
-                                   className='pnl-panel'/>
-            </div>
-          ],
-          cols: 2560,
-          rowHeight: 30,
-          width: 2560
-        }
-      }
-    )
+    this.set_tool_tip('asdfasf')
   }
 
   render() {
